615660d08dadbbec787f601064f7db41ae9398de
Albert G. <a.golovanov1323@gmail.com>
2025-12-23 13:53:07 +0600

    Implemented classes for working with resumes.

--- src/main/java/kg/attractor/job_search/dao/ResumeDao.java
+++ src/main/java/kg/attractor/job_search/dao/ResumeDao.java
@@ -2,27 +2,86 @@ package kg.attractor.job_search.dao;
 
 import kg.attractor.job_search.model.Resume;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class ResumeDao {
     private final JdbcTemplate jdbcTemplate;
+    KeyHolder keyHolder = new GeneratedKeyHolder();
 
     public List<Resume> getAllResumes() {
-        String sql = "SELECT * FROM resumes";
+        String sql = "SELECT * FROM RESUMES";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Resume.class));
     }
     public List<Resume> getList(Integer id) {
-        String sql = "SELECT * FROM resumes WHERE category_id = ?";
+        String sql = "SELECT * FROM RESUMES WHERE CATEGORY_ID = ?";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Resume.class), id);
     }
     public List<Resume> getListByApplicantId(Integer id) {
-        String sql = "SELECT * FROM resumes WHERE applicant_id = ?";
+        String sql = "SELECT * FROM RESUMES WHERE APPLICANT_ID = ?";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Resume.class), id);
+    }
+
+    public Optional<Resume> getById(Integer id) {
+        String sql = "SELECT * FROM RESUMES WHERE ID = ?";
+        return Optional.ofNullable(
+                DataAccessUtils.singleResult(
+                        jdbcTemplate.query(
+                                sql,
+                                new BeanPropertyRowMapper<>(Resume.class),
+                                id
+                        )
+                )
+        );
+    }
+
+    public void create(Resume resume){
+        String sql = "INSERT INTO RESUMES (NAME, SALARY, IS_ACTIVE, CREATED_DATE, UPDATE_TIME, APPLICANT_ID, CATEGORY_ID) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);;
+            ps.setString(1, resume.getName());
+            ps.setFloat(2, resume.getSalary());
+            ps.setBoolean(3, resume.isActive());
+            ps.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+            ps.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now()));
+            ps.setInt(6, resume.getApplicantId());
+            ps.setInt(7, resume.getCategoryId());
+            return ps;
+        }, keyHolder);
+
+        resume.setId(Objects.requireNonNull(keyHolder.getKey()).intValue());
+    }
+
+    public void edit(Resume resume) {
+        String sql = "UPDATE RESUMES SET NAME = ?, SALARY = ?, IS_ACTIVE = ?, UPDATE_TIME = ?, CATEGORY_ID = ? WHERE ID = ?";
+        jdbcTemplate.update(sql,
+                resume.getName(),
+                resume.getSalary(),
+                resume.isActive(),
+                Timestamp.valueOf(LocalDateTime.now()),
+                resume.getCategoryId(),
+                resume.getId()
+        );
+    }
+
+    public void deleteById(Integer resumeId) {
+        String sql = "DELETE FROM RESUMES WHERE ID = ?";
+        jdbcTemplate.update(sql, resumeId);
     }
 }
--- src/main/java/kg/attractor/job_search/service/impl/ResumeServiceImpl.java
+++ src/main/java/kg/attractor/job_search/service/impl/ResumeServiceImpl.java
@@ -1,12 +1,16 @@
 package kg.attractor.job_search.service.impl;
 
+import kg.attractor.job_search.dao.EducationInfoDao;
 import kg.attractor.job_search.dao.ResumeDao;
+import kg.attractor.job_search.dao.WorkExperienceInfoDao;
 import kg.attractor.job_search.dto.ResumeDto;
+import kg.attractor.job_search.exceptions.ResumeNotFoundException;
 import kg.attractor.job_search.model.Resume;
 import kg.attractor.job_search.service.ResumeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +18,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ResumeServiceImpl implements ResumeService {
     private final ResumeDao resumeDao;
+    private final EducationInfoDao educationInfoDao;
+    private final WorkExperienceInfoDao workExperienceInfoDao;
     @Override
     public List<ResumeDto> getList(Integer id) {
         List<Resume> resumes = resumeDao.getList(id);
@@ -32,6 +38,82 @@ public class ResumeServiceImpl implements ResumeService {
         return convertToDtos(resumes);
     }
 
+    @Override
+    public ResumeDto getById(Integer id) {
+        Resume resume = resumeDao.getById(id)
+                .orElseThrow(ResumeNotFoundException::new);
+        return convertToDto(resume);
+    }
+
+    @Override
+    public ResumeDto create(ResumeDto resumeDto) {
+        Resume resume = new Resume();
+        resume.setName(resumeDto.getName());
+        resume.setSalary(resumeDto.getSalary());
+        resume.setActive(true);
+        resume.setApplicantId(resumeDto.getApplicantId());
+        resume.setCategoryId(resumeDto.getCategoryId());
+        resumeDao.create(resume);
+
+        Integer resumeId = resume.getId();
+
+        if (resumeDto.getEducation() != null){
+            resumeDto.getEducation().forEach(e -> {
+                e.setResumeId(resumeId);
+                educationInfoDao.create(e);
+            });
+        }
+        if (resumeDto.getWorkExperience() != null){
+            resumeDto.getWorkExperience().forEach(w -> {
+                w.setResumeId(resumeId);
+                workExperienceInfoDao.create(w);
+            });
+        }
+        return resumeDao.getById(resumeId)
+                .map(this::convertToDto)
+                .orElseThrow(ResumeNotFoundException::new);
+    }
+
+    public ResumeDto edit(Integer resumeId, ResumeDto resumeDto) {
+        Resume resume = resumeDao.getById(resumeId)
+                .orElseThrow(ResumeNotFoundException::new);
+
+        if(resumeDto.getName() != null) resume.setName(resumeDto.getName());
+        if(resumeDto.getSalary() != 0) resume.setSalary(resumeDto.getSalary());
+        if(resumeDto.getCategoryId() != null) resume.setCategoryId(resumeDto.getCategoryId());
+
+        resumeDao.edit(resume);
+
+        educationInfoDao.deleteByResumeId(resumeId);
+        if(resumeDto.getEducation() != null){
+            resumeDto.getEducation().forEach(e -> {
+                e.setResumeId(resumeId);
+                educationInfoDao.create(e);
+            });
+        }
+
+        workExperienceInfoDao.deleteByResumeId(resumeId);
+        if(resumeDto.getWorkExperience() != null){
+            resumeDto.getWorkExperience().forEach(w -> {
+                w.setResumeId(resumeId);
+                workExperienceInfoDao.create(w);
+            });
+        }
+
+        return convertToDto(resume);
+    }
+
+    @Override
+    public void update(Integer id, ResumeDto resumeDto) {
+        resumeDto.setUpdateTime(LocalDateTime.now());
+    }
+
+    public void delete(Integer resumeId) {
+        Resume resume = resumeDao.getById(resumeId)
+                .orElseThrow(ResumeNotFoundException::new);
+        resumeDao.deleteById(resumeId);
+    }
+
     private List<ResumeDto> convertToDtos(List<Resume> resumes) {
         List<ResumeDto> resumeDtos = new ArrayList<>();
         resumes.forEach(resume -> {
@@ -44,8 +126,25 @@ public class ResumeServiceImpl implements ResumeService {
             resumeDto.setActive(resume.isActive());
             resumeDto.setCreatedDate(resume.getCreatedDate());
             resumeDto.setUpdateTime(resume.getUpdateTime());
+            resumeDto.setEducation(educationInfoDao.getListByResumeId(resume.getId()));
+            resumeDto.setWorkExperience(workExperienceInfoDao.getListByResumeId(resume.getId()));
             resumeDtos.add(resumeDto);
         });
         return resumeDtos;
     }
+    private ResumeDto convertToDto(Resume resume) {
+            ResumeDto resumeDto = new ResumeDto();
+            resumeDto.setId(resume.getId());
+            resumeDto.setApplicantId(resume.getApplicantId());
+            resumeDto.setCategoryId(resume.getCategoryId());
+            resumeDto.setName(resume.getName());
+            resumeDto.setSalary(resume.getSalary());
+            resumeDto.setActive(resume.isActive());
+            resumeDto.setCreatedDate(resume.getCreatedDate());
+            resumeDto.setUpdateTime(resume.getUpdateTime());
+            resumeDto.setEducation(educationInfoDao.getListByResumeId(resume.getId()));
+            resumeDto.setWorkExperience(workExperienceInfoDao.getListByResumeId(resume.getId()));
+        return resumeDto;
+    }
+
 }
