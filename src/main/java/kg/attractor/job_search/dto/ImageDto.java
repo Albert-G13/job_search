12e60e1269ab26090801478316c5b730285ef200
Albert G. <a.golovanov1323@gmail.com>
2025-12-26 18:05:17 +0600

    Added validation annotations for image

--- src/main/java/kg/attractor/job_search/dto/ImageDto.java
+++ src/main/java/kg/attractor/job_search/dto/ImageDto.java
@@ -1,5 +1,7 @@
 package kg.attractor.job_search.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,6 +9,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 @Builder
 public class ImageDto {
+    @NotNull(message = "File is required")
     private MultipartFile file;
+    @Positive(message = "Movie id must be positive")
     private long movieId;
 }
