5cb3184dc84b87b03ed003e5dbddb3f0dcc77e5b
Albert G. <a.golovanov1323@gmail.com>
2025-12-26 18:04:46 +0600

    Added validation annotations for educationInfo

--- src/main/java/kg/attractor/job_search/dto/EducationInfoDto.java
+++ src/main/java/kg/attractor/job_search/dto/EducationInfoDto.java
@@ -1,5 +1,8 @@
 package kg.attractor.job_search.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,10 +10,18 @@ import java.time.LocalDateTime;
 @Data
 public class EducationInfoDto {
     private Integer id;
+    @NotNull(message = "Resume id is required")
     private Integer resumeId;
+    @NotBlank(message = "Institution is required")
+    @Size(max = 100)
     private String institution;
+    @NotBlank(message = "Program is required")
+    @Size(max = 100)
     private String program;
+    @NotNull(message = "Start date is required")
     private LocalDateTime startDate;
+    @NotNull(message = "End date is required")
     private LocalDateTime endDate;
+    @NotBlank(message = "Degree is required")
     private String degree;
 }
--- src/main/resources/db/changelog/initial/013_insert_resumes.sql
+++ src/main/resources/db/changelog/initial/013_insert_resumes.sql
@@ -1,3 +1,3 @@
 INSERT INTO resumes (name, salary, is_active, created_date, update_time, applicant_id, category_id)
-VALUES ('Middle developer', 95000, TRUE, '2025-12-17 08:35:00', '2025-12-17 08:50:00', 1, 1),
-       ('Middle developer', 100000, TRUE, '2025-12-18 20:35:00', '2025-12-18 21:50:00', 1, 2);
+VALUES ('Middle developer', 95000, TRUE, '2025-12-17 08:35:00', '2025-12-17 08:50:00', select id from USERS_TABLE where USERS_TABLE.EMAIL = 'kostya.bro@mail.ru', select id from CATEGORIES where CATEGORIES.NAME = 'IT'),
+       ('Middle developer', 100000, TRUE, '2025-12-18 20:35:00', '2025-12-18 21:50:00', select id from USERS_TABLE where USERS_TABLE.EMAIL = 'kostya.bro@mail.ru', select id from CATEGORIES where CATEGORIES.NAME = 'Software Development');
