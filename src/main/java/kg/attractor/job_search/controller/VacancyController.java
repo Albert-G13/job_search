46463140b36261113f6dfc5ce8443c119b5b8ef7
Albert G. <a.golovanov1323@gmail.com>
2025-12-26 18:06:28 +0600

    Added validation annotations for vacancy

--- src/main/java/kg/attractor/job_search/controller/VacancyController.java
+++ src/main/java/kg/attractor/job_search/controller/VacancyController.java
@@ -1,5 +1,6 @@
 package kg.attractor.job_search.controller;
 
+import jakarta.validation.Valid;
 import kg.attractor.job_search.dto.UserDto;
 import kg.attractor.job_search.dto.VacancyDto;
 import kg.attractor.job_search.service.VacancyService;
@@ -10,46 +11,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vacancies")
+@RequestMapping("/employer")
 @RequiredArgsConstructor
 public class VacancyController {
     private final VacancyService vacancyService;
 
-    @GetMapping("/{id}")
+    @GetMapping("/vacancies/{id}")
     public VacancyDto getVacancyById(@PathVariable Integer id){
         return vacancyService.getById(id);
     }
-    @GetMapping("/applicant/{applicantId}")
+    @GetMapping("/vacancies/applicant/{applicantId}")
     public List<VacancyDto> getVacanciesByRespondedId(@PathVariable Integer applicantId) {
         return vacancyService.getVacanciesByRespondedId(applicantId);
     }
-    @GetMapping
+    @GetMapping("/vacancies")
     public List<VacancyDto> getAllVacancies() {
         return vacancyService.getAllVacancies();
     }
-    @GetMapping("/category/{categoryId}")
+    @GetMapping("/vacancies/category/{categoryId}")
     public List<VacancyDto> getVacanciesByCategoryId(@PathVariable Integer categoryId) {
         return vacancyService.getVacanciesByCategoryId(categoryId);
     }
-    @GetMapping("/{vacancyId}/responded")
+    @GetMapping("/vacancies/{vacancyId}/responded")
     public List<UserDto> getRespondedApplicantsByVacancyId(@PathVariable Integer vacancyId){
         return vacancyService.getRespondedApplicantsByVacancyId(vacancyId);
     }
-    @PostMapping("/create")
-    public Integer createVacancy(@RequestBody VacancyDto vacancyDto){
+    @PostMapping("/vacancies")
+    public Integer createVacancy(@RequestBody @Valid VacancyDto vacancyDto){
         return vacancyService.create(vacancyDto);
     }
-    @PutMapping("/{id}")
-    public HttpStatus editVacancy(@PathVariable Integer id, @RequestBody VacancyDto vacancyDto){
+    @PutMapping("/vacancies/{id}")
+    public HttpStatus editVacancy(@PathVariable Integer id, @RequestBody @Valid VacancyDto vacancyDto){
         vacancyService.edit(id, vacancyDto);
         return HttpStatus.ACCEPTED;
     }
-    @PutMapping("/{id}/update")
-    public HttpStatus updateVacancy(@PathVariable Integer id, @RequestBody VacancyDto vacancyDto){
+    @PutMapping("/vacancies/{id}/update")
+    public HttpStatus updateVacancy(@PathVariable Integer id, @RequestBody @Valid VacancyDto vacancyDto){
         vacancyService.update(id, vacancyDto);
         return HttpStatus.ACCEPTED;
     }
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/vacancies/{id}")
     public HttpStatus deleteVacancy(@PathVariable Integer id) {
         vacancyService.delete(id); return HttpStatus.OK;
     }
--- src/main/java/kg/attractor/job_search/dto/VacancyDto.java
+++ src/main/java/kg/attractor/job_search/dto/VacancyDto.java
@@ -1,17 +1,27 @@
 package kg.attractor.job_search.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 @Data
 public class VacancyDto {
     private Integer id;
+    @NotNull
     private Integer categoryId;
+    @NotNull
     private Integer authorId;
+    @NotBlank
+    @Size(min = 3, max = 100)
     private String name;
+    @NotBlank
+    @Size(min = 3, max = 2000)
     private String description;
+    @Positive
     private float salary;
+    @Min(0)
     private int expFrom;
+    @Min(0)
     private int expTo;
     private boolean isActive;
     private LocalDateTime createdDate;
