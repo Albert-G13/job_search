6a678d3f0b68c4191d1c3ea99d568a7df9377809
Albert G. <a.golovanov1323@gmail.com>
2026-01-20 18:12:34 +0600

    Implemented resume addition and editing

--- src/main/java/kg/attractor/job_search/controller/ResumeController.java
+++ src/main/java/kg/attractor/job_search/controller/ResumeController.java
@@ -1,11 +1,14 @@
 package kg.attractor.job_search.controller;
 
+import jakarta.validation.Valid;
+import kg.attractor.job_search.dto.ResumeDto;
+import kg.attractor.job_search.dto.ResumeEditDto;
 import kg.attractor.job_search.service.ResumeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/resumes")
@@ -17,5 +20,40 @@ public class ResumeController {
     public String getResumes(Model model){
         model.addAttribute("resumes", resumeService.getAllResumes());
         return "resumes/resume";
+    }
+
+    @GetMapping("/create/{applicantId}")
+    public String createGet(Model model, @PathVariable Integer applicantId) {
+        model.addAttribute("resume", new ResumeDto());
+        model.addAttribute("applicantId", applicantId);
+        return "resumes/create";
+    }
+
+    @PostMapping("/create/{applicantId}")
+    public String createPost(@Valid @ModelAttribute("resume") ResumeDto dto, BindingResult bindingResult, @PathVariable Integer applicantId, Model model) {
+        if (bindingResult.hasErrors()){
+            model.addAttribute("applicantId", applicantId);
+            return "resumes/create";
+        }
+
+        resumeService.create(dto, applicantId);
+        return "redirect:/resumes";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editGet(@PathVariable Integer id, Model model) {
+        model.addAttribute("resume", resumeService.getById(id));
+        return "resumes/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String editPost(@PathVariable Integer id, @Valid @ModelAttribute("resume") ResumeEditDto dto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()){
+            dto.setId(id);
+            return "resumes/edit";
+        }
+
+        resumeService.edit(id, dto);
+        return "redirect:/resumes";
     }
 }
