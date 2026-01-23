9a79dfccc2d1faccc83e112369f9bb556edad2d9
Albert G. <a.golovanov1323@gmail.com>
2026-01-23 17:53:52 +0600

    Changed the entity Education, ContactType, ContactInfo, and Category.

--- src/main/java/kg/attractor/job_search/model/Category.java
+++ src/main/java/kg/attractor/job_search/model/Category.java
@@ -1,12 +1,18 @@
 package kg.attractor.job_search.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private Integer parentId;
     private String name;
 }
--- src/main/java/kg/attractor/job_search/model/ContactType.java
+++ src/main/java/kg/attractor/job_search/model/ContactType.java
@@ -1,11 +1,17 @@
 package kg.attractor.job_search.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "contact_types")
 public class ContactType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String contactType;
 }
