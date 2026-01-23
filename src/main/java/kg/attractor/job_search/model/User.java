00e05afdc900a28c3ef985000d6dd168b2cd0e28
Albert G. <a.golovanov1323@gmail.com>
2026-01-23 17:49:12 +0600

    Added the Hibernate dependency and changed the User entity

--- src/main/java/kg/attractor/job_search/model/User.java
+++ src/main/java/kg/attractor/job_search/model/User.java
@@ -1,5 +1,6 @@
 package kg.attractor.job_search.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -7,16 +8,28 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "users_table", schema = "public")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String surname;
     private Integer age;
     private String email;
     private String password;
+
+    @Column(name = "PHONE_NUMBER")
     private String phoneNumber;
+
     private String avatar;
     private String role;
-    private Integer roleId;
+
+    @ManyToOne
+    @JoinColumn(name = "ROLE_ID")
+    private Role roleId;
+
     private boolean enabled;
 }
